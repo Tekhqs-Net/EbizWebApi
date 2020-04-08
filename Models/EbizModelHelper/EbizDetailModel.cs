039ab476e7e7b111b3d87aeb23007d59f0727f41
Naqi Hussain <naqi@uneekclothing.com>
2020-04-08 18:56:41 +0500

    ok08-04-20

--- Models/EbizModelHelper/EbizDetailModel.cs
+++ Models/EbizModelHelper/EbizDetailModel.cs
@@ -41,5 +41,55 @@ namespace WebApi.Models.EbizModelHelper
         public dynamic TransResponse { get; set; }
         public dynamic CustomerResponse { get; set; }
         public dynamic PaymentMethodProfileResponse { get; set; }
+        public dynamic SearchTransResponse { get; set; }
+    }
+    public class SearchTransactionDetailModel
+    {
+        public SearchFilter[] searchFilters { get; set; }
+        public bool matchAll { get; set; }
+        public bool countOnly { get; set; }
+        public string start  { get; set; }
+        public string limit { get; set; }
+        public string sort { get; set; }
+        public SecurityToken token { get; set; }
+    }
+    public class GetTransactionDetailModel
+    {
+        public SecurityToken token { get; set; }
+        public string transactionRefNum { get; set; }
+    }
+        public class TransactionReturnModel
+    {
+        public List<TransactionObject> transactionObjects { get; set; }
+    }
+    public class CustomerReturnModel
+    {
+        public List<Customer> customers { get; set; }
+    }
+    public class OrderDetailModel
+    {
+        public string customerId { get; set; }
+        public string subCustomerId { get; set; }
+        public string salesOrderNo { get; set; }
+        public string salesOrderInternalId { get; set; }
+        public SearchFilter[] searchFilters { get; set; }
+        public bool includeItems { get; set; }
+        public int start { get; set; }
+        public int limit { get; set; }
+        public string sort { get; set; }
+        public SecurityToken token { get; set; }
+}
+    public class CustomerDetailModel
+    {
+        public SecurityToken token { get; set; }
+        public string customerId { get; set; }
+        public string customerInternalId { get; set; }
+        public int start { get; set; }
+        public int limit { get; set; }
+        public string sort { get; set; }
+        public bool includeCustomerToken { get; set; }
+        public bool includePaymentMethodProfile { get; set; }
+        public bool countOnly { get; set; }
+        public SearchFilter[] searchFilters { get; set; }
     }
 }
